0e11085ab0eac23f3f2ef19ba8062c380ff1f7f1
Danil Golybev <58464440+danilksk@users.noreply.github.com>
2021-03-16 12:31:01 +0300

    Add files via upload

---
+++ gmail.hack.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import time
+import socket
+import random
+id = input(" Target gmail: ")
+os.system("clear")
+print ("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥	 wait Hack password♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+
+time.sleep(10)
+print ("gmail password is wait --------> 0% ")
+time.sleep(10)
+print ("gmail password is wait --------> 10%")
+time.sleep(10)
+print ("gmail password is wait --------> 20% ")
+time.sleep(10)
+print ("gmail password is wait --------> 35% ")
+time.sleep(10)
+print ("gmail password is wait --------> 50% ")
+time.sleep(10)
+print ("gmail password is wait --------> 65% ")
+time.sleep(10)
+print ("gmail password is wait --------> 75% ")
+time.sleep(10)
+print ("gmail password is wait --------> 85% ")
+time.sleep(10)
+print ("gmail password is wait --------> 99% ")
+time.sleep(10)
+print ("gmail password is wait --------> 100% ")
+time.sleep(10)
+#os.system("clear")
+print ("ты рил думаешь, что это будет так просто? нет, иди нахер, чмо)")
+
